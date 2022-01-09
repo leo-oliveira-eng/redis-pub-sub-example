ca87120af1fe9c076961916cf1ba598ce708accc
Leonardo de Oliveira <leo.oliveira.eng@outlook.com>
2022-01-09 16:03:44 -0300

    Basic structure for registration

---
+++ src/Registration/RedisExample.Registration.WebApi/Controllers/Base/MeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RedisExample.Registration.WebApi.Controllers.Base
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class MeController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get() => Ok(new { name = "Redis Example - Registration - Web API", version = "1.0.0" });
+    }
+}
