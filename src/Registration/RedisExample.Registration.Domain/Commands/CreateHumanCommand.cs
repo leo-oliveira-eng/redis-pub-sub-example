4835e1aacf13ae9bb3914e2cda333ca083addacb
Leonardo de Oliveira <leo.oliveira.eng@outlook.com>
2022-01-11 16:18:58 -0300

    implementing create human command

---
+++ src/Registration/RedisExample.Registration.Domain/Commands/CreateHumanCommand.cs
@@ -0,0 +1,40 @@
+﻿using Messages.Core;
+using Messages.Core.Extensions;
+using RedisExample.Registration.Domain.Enums;
+using Valuables.Utils;
+
+namespace RedisExample.Registration.Domain.Commands
+{
+    public class CreateHumanCommand : HumanCommand
+    {
+        public string? Cpf { get; set; }
+
+        public string? Name { get; set; }
+
+        public DateTime BirthDate { get; private set; }
+
+        public GenderType Gender { get; private set; }
+
+        public override Response Validate()
+        {
+            var response = base.Validate();
+
+            if (!CPF.IsValid(Cpf))
+                response.WithBusinessError(nameof(Cpf), $"{nameof(Cpf)} is invalid");
+
+            if (BirthDateIsValid(BirthDate))
+                response.WithBusinessError(nameof(BirthDate), $"{nameof(BirthDate)} is invalid");
+
+            if (string.IsNullOrEmpty(Name))
+                response.WithBusinessError(nameof(Name), $"{nameof(Name)} is invalid");
+
+            if (!Enum.IsDefined(typeof(GenderType), Gender))
+                response.WithBusinessError(nameof(Gender), $"{nameof(Gender)} is invalid");
+
+            return response;
+        }
+
+        private static bool BirthDateIsValid(DateTime birthDate)
+            => birthDate.Date <= DateTime.Today.AddYears(-18) && birthDate > DateTime.Today.AddYears(-120);
+    }
+}
---
+++ src/Registration/RedisExample.Registration.Domain/Commands/HumanCommand.cs
@@ -0,0 +1,36 @@
+﻿using BaseEntity.Domain.Messaging;
+using MediatR;
+using Messages.Core;
+using Messages.Core.Extensions;
+using RedisExample.Registration.Domain.Commands.Dtos;
+using RedisExample.Registration.Domain.Models;
+
+namespace RedisExample.Registration.Domain.Commands
+{
+    public class HumanCommand : Command, IRequest<Response<Human>>
+    {
+        public string? Email { get; set; }
+
+        public string? PhoneNumber { get; set; }
+
+        public AddressDto? Address { get; set; }
+
+        public override Response Validate()
+        {
+            var response = Response.Create();
+
+            if (!Valuables.Utils.Email.IsValid(Email))
+                response.WithBusinessError(nameof(Email), $"{nameof(Email)} is invalid");
+
+            if (string.IsNullOrWhiteSpace(PhoneNumber))
+                response.WithBusinessError(nameof(PhoneNumber),$"{nameof(PhoneNumber)} is invalid");
+
+            var addressIsValidResponse = Valuables.Utils.Address.IsValid(Address?.Cep, Address?.Street, Address?.Neighborhood, Address?.Number, Address?.City, Address?.UF);
+
+            if (addressIsValidResponse.HasError)
+                response.WithMessages(addressIsValidResponse.Messages);
+
+            return response;
+        }
+    }
+}
