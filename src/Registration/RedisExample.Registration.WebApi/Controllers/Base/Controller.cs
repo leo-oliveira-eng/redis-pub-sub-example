5c29260e596d68d4d5999df30b3aa38e7faee3e2
Leonardo de Oliveira <leo.oliveira.eng@outlook.com>
2022-01-13 12:44:22 -0300

    implementing base controller

---
+++ src/Registration/RedisExample.Registration.WebApi/Controllers/Base/Controller.cs
@@ -0,0 +1,48 @@
+﻿using Messages.Core;
+using Messages.Core.Enums;
+using Microsoft.AspNetCore.Mvc;
+
+namespace RedisExample.Registration.WebApi.Controllers.Base
+{
+    public abstract class Controller : ControllerBase
+    {
+        protected async Task<IActionResult> WithResponseAsync<TResponseMessage>(Func<Task<Response<TResponseMessage>>> func)
+        {
+            var response = await func.Invoke();
+
+            if (!response.HasError)
+                return Ok(response);
+
+            if (response.Messages.Any(m => m.Type == MessageType.BusinessError))
+                return BadRequest(response);
+
+            return StatusCode(500, response);
+        }
+
+        protected async Task<IActionResult> WithResponse(Func<Task<Response>> func)
+        {
+            var response = await func.Invoke();
+
+            if (!response.HasError)
+                return Ok(response);
+
+            if (response.Messages.Any(m => m.Type == MessageType.BusinessError))
+                return BadRequest(response);
+
+            return StatusCode(500, response);
+        }
+
+        protected IActionResult WithResponse(Func<Response> func)
+        {
+            var response = func.Invoke();
+
+            if (!response.HasError)
+                return Ok(response);
+
+            if (response.Messages.Any(m => m.Type == MessageType.BusinessError))
+                return BadRequest(response);
+
+            return StatusCode(500, response);
+        }
+    }
+}
