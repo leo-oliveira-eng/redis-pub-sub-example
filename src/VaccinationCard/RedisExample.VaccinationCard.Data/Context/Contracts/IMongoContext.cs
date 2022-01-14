ce8341e77360c8bd7ac334c311dc4fbffaea33c3
Leonardo de Oliveira <leo.oliveira.eng@outlook.com>
2022-01-14 18:25:30 -0300

    implementing context for mongodb

---
+++ src/VaccinationCard/RedisExample.VaccinationCard.Data/Context/Contracts/IMongoContext.cs
@@ -0,0 +1,9 @@
+﻿using MongoDB.Driver;
+
+namespace RedisExample.VaccinationCard.Data.Context.Contracts
+{
+    public interface IMongoContext
+    {
+        IMongoCollection<T> GetCollection<T>(string name);
+    }
+}
---
+++ src/VaccinationCard/RedisExample.VaccinationCard.Data/Context/MongoContext.cs
@@ -0,0 +1,23 @@
+﻿using MongoDB.Driver;
+using RedisExample.VaccinationCard.Common.Settings.Contracts;
+using RedisExample.VaccinationCard.Data.Context.Contracts;
+
+namespace RedisExample.VaccinationCard.Data.Context
+{
+    public class MongoContext : IMongoContext
+    {
+        private IMongoDatabase Database { get; }
+
+        public MongoClient MongoClient { get; set; }
+
+        public MongoContext(ISettings settings)
+        {
+            MongoClient = new MongoClient(settings.MongoDbSettings.ConnectionString);
+
+            Database = MongoClient.GetDatabase(settings.MongoDbSettings.DatabaseName);
+        }
+
+        public IMongoCollection<T> GetCollection<T>(string name)
+            => Database.GetCollection<T>(name);
+    }
+}
