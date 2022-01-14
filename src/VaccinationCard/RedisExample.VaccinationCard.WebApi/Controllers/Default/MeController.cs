d3bb64310e6275cc663171bb0294c9306bad4b19
Leonardo de Oliveira <leo.oliveira.eng@outlook.com>
2022-01-14 12:58:28 -0300

    Basic structure for vaccination card

---
+++ src/VaccinationCard/RedisExample.VaccinationCard.WebApi/Controllers/Default/MeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RedisExample.VaccinationCard.WebApi.Controllers.Default
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class MeController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get() => Ok(new { name = "Redis Example - Vaccination Card - Web API", version = "1.0.0" });
+    }
+}
