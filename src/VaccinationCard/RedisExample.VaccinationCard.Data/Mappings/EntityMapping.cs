a0063fc012b4cf742fe6f1404494cf3c81d5a73d
Leonardo de Oliveira <leo.oliveira.eng@outlook.com>
2022-01-15 08:29:36 -0300

    implementing entity mapping

---
+++ src/VaccinationCard/RedisExample.VaccinationCard.Data/Mappings/EntityMapping.cs
@@ -0,0 +1,22 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization;
+using MongoDB.Bson.Serialization.Serializers;
+using RedisExample.VaccinationCard.Domain.Models;
+
+namespace RedisExample.VaccinationCard.Data.Mappings
+{
+    public static class EntityMapping
+    {
+        public static void MapEntity()
+        {
+            BsonClassMap.RegisterClassMap<Entity>(cm =>
+            {
+                BsonSerializer.RegisterSerializer(typeof(Guid), new GuidSerializer(BsonType.String));
+
+                cm.AutoMap();
+
+                cm.MapIdMember(x => x.Id);
+            });
+        }
+    }
+}
