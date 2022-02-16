4ab335f09f09c28b53314a64b9ef3563bf066616
Leonardo de Oliveira <leo.oliveira.eng@outlook.com>
2022-02-16 20:03:38 -0300

    overriding findAsync

--- src/Registration/RedisExample.Registration.Persistence/Repositories/HumanRepository.cs
+++ src/Registration/RedisExample.Registration.Persistence/Repositories/HumanRepository.cs
@@ -1,4 +1,7 @@
-﻿using Infrastructure.Repositories;
+﻿#pragma warning disable CS8604 // Possible null reference argument.
+using Infrastructure.Repositories;
+using Messages.Core;
+using Microsoft.EntityFrameworkCore;
 using RedisExample.Registration.Domain.Models;
 using RedisExample.Registration.Domain.Repositories;
 using RedisExample.Registration.Persistence.Context;
@@ -8,5 +11,12 @@ namespace RedisExample.Registration.Persistence.Repositories
     public class HumanRepository : Repository<Human, RegistrationContext>, IHumanRepository
     {
         public HumanRepository(RegistrationContext context) : base(context) { }
+
+        public override async Task<Maybe<Human>> FindAsync(Guid code)
+            => await DbSet
+                .Include(human => human.Pets)
+                    .ThenInclude(pet => pet.Vaccines)
+                .SingleOrDefaultAsync(human => human.Code.Equals(code));
     }
 }
+#pragma warning restore CS8604 // Possible null reference argument.
