d49e5dd4d27ff01867f3b39a4822f2afe0dfb2f4
Leonardo de Oliveira <leo.oliveira.eng@outlook.com>
2022-02-22 22:58:56 -0300

    code coverage for Human created event handler

---
+++ src/Registration/RedisExample.Registration.Application.Tests/HandlerTests/HumanCreatedEventHandlerUnitTests.cs
@@ -0,0 +1,51 @@
+﻿using FluentAssertions;
+using Moq;
+using RedisExample.Registration.Application.EventHandlers;
+using RedisExample.Registration.Application.EventHandlers.Contracts;
+using RedisExample.Registration.Application.Tests.Shared;
+using RedisExample.Registration.Domain.Events;
+using System;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace RedisExample.Registration.Application.Tests.HandlerTests
+{
+    public class HumanCreatedEventHandlerUnitTests : BaseMock
+    {
+        private readonly Mock<IEventPublisher> _eventPublisher = new();
+
+        private HumanCreatedEventHandler Handler { get; }
+
+        public HumanCreatedEventHandlerUnitTests()
+        {
+            Handler = new HumanCreatedEventHandler(_eventPublisher.Object);
+        }
+
+        [Fact]
+        public async Task Handle_ShouldThrowArgumentNullException_EventIsNull()
+        {
+            _eventPublisher.Setup(publisher => publisher.Publish(It.IsAny<HumanCreatedEvent>()));
+
+            HumanCreatedEvent? @event = null;
+
+            var response = await Record.ExceptionAsync(() => Handler.Handle(@event!));
+
+            response.Should().NotBeNull();
+            response.Should().BeOfType(typeof(ArgumentNullException));
+            response.Message.Should().Be("Value cannot be null. (Parameter '@event')");
+            _eventPublisher.Verify(publisher => publisher.Publish(It.IsAny<HumanCreatedEvent>()), Times.Never);
+        }
+
+        [Fact]
+        public async Task Handle_Success_ShouldPublishEvent()
+        {
+            _eventPublisher.Setup(publisher => publisher.Publish(It.IsAny<HumanCreatedEvent>()));
+
+            var @event = new HumanCreatedEvent(HumanFake());
+
+            await Handler.Handle(@event);
+
+            _eventPublisher.Verify(publisher => publisher.Publish(@event), Times.Once);
+        }
+    }
+}
