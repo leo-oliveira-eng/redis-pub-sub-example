3721d3385c540368f9fd6ebda72cf2d57c564a2b
Leonardo de Oliveira <leo.oliveira.eng@outlook.com>
2022-03-14 20:27:49 -0300

    implementing human deleted event handler

---
+++ src/Registration/RedisExample.Registration.Application.Tests/HandlerTests/HumanDeletedEventHandlerUnitTests.cs
@@ -0,0 +1,51 @@
+﻿using FluentAssertions;
+using Moq;
+using RedisExample.Registration.Application.EventHandlers;
+using RedisExample.Registration.Application.EventHandlers.Contracts;
+using RedisExample.Registration.Application.Tests.Shared;
+using RedisExample.Registration.Domain.Events;
+using System;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace RedisExample.Registration.Application.Tests.HandlerTests
+{
+    public class HumanDeletedEventHandlerUnitTests : BaseMock
+    {
+        private readonly Mock<IEventPublisher> _eventPublisher = new();
+
+        private HumanDeletedEventHandler Handler { get; }
+
+        public HumanDeletedEventHandlerUnitTests()
+        {
+            Handler = new HumanDeletedEventHandler(_eventPublisher.Object);
+        }
+
+        [Fact]
+        public async Task Handle_ShouldThrowArgumentNullException_EventIsNull()
+        {
+            _eventPublisher.Setup(publisher => publisher.PublishAsync(It.IsAny<HumanDeletedEvent>()));
+
+            HumanDeletedEvent? @event = null;
+
+            var response = await Record.ExceptionAsync(() => Handler.Handle(@event!));
+
+            response.Should().NotBeNull();
+            response.Should().BeOfType(typeof(ArgumentNullException));
+            response.Message.Should().Be("Value cannot be null. (Parameter '@event')");
+            _eventPublisher.Verify(publisher => publisher.PublishAsync(It.IsAny<HumanDeletedEvent>()), Times.Never);
+        }
+
+        [Fact]
+        public async Task Handle_Success_ShouldPublishEvent()
+        {
+            _eventPublisher.Setup(publisher => publisher.PublishAsync(It.IsAny<HumanDeletedEvent>()));
+
+            var @event = new HumanDeletedEvent(Guid.NewGuid());
+
+            await Handler.Handle(@event);
+
+            _eventPublisher.Verify(publisher => publisher.PublishAsync(@event), Times.Once);
+        }
+    }
+}
---
+++ src/Registration/RedisExample.Registration.Application/EventHandlers/HumanDeletedEventHandler.cs
@@ -0,0 +1,23 @@
+﻿using MediatR;
+using RedisExample.Registration.Application.EventHandlers.Contracts;
+using RedisExample.Registration.Domain.Events;
+
+namespace RedisExample.Registration.Application.EventHandlers
+{
+    public class HumanDeletedEventHandler : INotificationHandler<HumanDeletedEvent>
+    {
+        private IEventPublisher EventPublisher { get; }
+
+        public HumanDeletedEventHandler(IEventPublisher eventPublisher)
+        {
+            EventPublisher = eventPublisher ?? throw new ArgumentNullException(nameof(eventPublisher));
+        }
+
+        public virtual Task Handle(HumanDeletedEvent @event, CancellationToken cancellationToken = default)
+        {
+            ArgumentNullException.ThrowIfNull(@event);
+
+            return EventPublisher.PublishAsync(@event);
+        }
+    }
+}
